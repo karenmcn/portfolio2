18f4645380328b45621097ec8b5cdd5e66cfa7fa
karenmcn <karen.mc.nelson@gmail.com>
2014-10-13 20:01:52 +0000

    trying to commit portfolio

---
+++ Portfolio2/portfolio.js
@@ -0,0 +1,14 @@
+/**
+* Created with Portfolio.
+* User: karenmcn
+* Date: 2014-10-13
+* Time: 04:43 PM
+* To change this template use Tools | Templates.
+*/
+$(document).ready(function(){
+  var logo = $("#logo");
+  $("#logo").hover(function(){
+  
+  $("#navwords").fadeToggle(); 
+  });
+});
